ea2dc5bbfe34b5e011533cce1914e8ce24a86359
Ravi Gulia <ravigulia@bitbucket.org>
2021-11-11 13:19:09 +0530

    mcqt

--- src/migrations/2021_11_29_999998_add_indexing_to_mcqt_table.php
+++ src/migrations/2021_11_29_999998_add_indexing_to_mcqt_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class addIndexingMcqtTable extends Migration
+class addIndexingToMcqtTable extends Migration
 {
     /**
      * Run the migrations.
